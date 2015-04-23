d10f24c2b8e8911f745f8f6c44b4efbe3b26173a
hasintha <hasintha.12@gmail.com>
2015-04-23 06:19:41 +0530

    student enrollment form

--- moodle/student.php
+++ moodle/student.php
@@ -1,54 +1,77 @@
- <!DOCTYPE html>
+<?php
+session_start();
+?> 
+
+<!DOCTYPE html>
 <html>
-<head>
-<title>student</title>
-</head>
-<body>
+    <head>
+        <title>student</title>
+    </head>
+    <body>
 
-<h1>Student Enrollment</h1>
-<div>
-  
-    <?php
-     $servername="localhost";
-     $username="root";
-     $password="";
-     $dbname="moodle";
-     
-     //Create connection
-     $conn=  mysqli_connect($servername, $username, $password, $dbname);
-     //check connection
-     if(!$conn){
-         
-         die("Connection failed:" .  mysqli_connect_error());
-     }
-     $sql="SELECT course_id,course_name FROM module";
-     $result= mysqli_query($conn, $sql);
-     
-     if(mysqli_num_rows($result)>0){
-         
-        echo "<table><tr><th>ID</th><th>Course</th><th> </th></tr>";//table h
-         
-         
-        while($row=  mysqli_fetch_assoc($result)){
-             echo "<tr><td>".$row["course_id"]."</td><td>".$row["course_name"];
-             echo( "</td><td>".'-><a href="http://mylink.com/path/">Enroll</a>'."</td></tr>" );
-            
-            
-        } 
-         
-     }
-     else{
-         echo "0 results";
-         
-     }
-     mysqli_close($conn);
-    ?>
-    
-</div>
+        <h1>Student Enrollment</h1>
+        <div>
+
+            <?php
+            $_SESSION["user"] = "120526L";
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "moodle";
+
+            //Create connection
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+            //check connection
+            if (!$conn) {
+
+                die("Connection failed:" . mysqli_connect_error());
+            }
+            $sql = "SELECT course_id,course_name FROM module";
+            $result = mysqli_query($conn, $sql);
+
+            if (mysqli_num_rows($result) > 0) {
+
+                echo "<table><tr><th>ID</th><th>Course</th><th> </th></tr>"; //table h
 
 
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo '<tr><td>' . $row["course_id"] . '</td><td>' . $row["course_name"];
+                    echo( '</td><td>-><a href="student.php?id=' . "'" . $row["course_id"] . "'" . '">Enroll</a></td></tr>' );
+                }
 
-</body>
+                echo ($_SESSION["user"]);
+            } else {
+                echo "0 results";
+            }
+            mysqli_close($conn);
+            ?>
+            <div>
+                <?php
+                $enrolled = $_GET['id'];
+                if ($enrolled != '0') {
+                    echo $enrolled;
+
+                    $conn = new mysqli('localhost', 'root', '', 'moodle');
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                    }
+                    $sql = "INSERT INTO enrollment (course_id,student_id)VALUES (".$enrolled.",'".$_SESSION["user"]."')";
+
+                    if ($conn->query($sql) === TRUE) {
+                        echo "Enrollent successfull";
+                    } else {
+                        echo "Error: " . $sql . "<br>" . $conn->error;
+                    }
+
+                    $conn->close();
+                }
+                ?>
+            </div>
+        </div>
+
+
+
+    </body>
 </html> 
 
 
