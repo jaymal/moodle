5b4147c33af95fe4fd2c6f989d143532f05bcb96
hasintha12 <hasintha.samith@gmail.com>
2015-06-16 01:58:17 +0530

    student deleted

--- moodle/students.php
+++ moodle/students.php
@@ -116,59 +116,7 @@ if (strcmp($_SESSION["access"], "student") != 0) {
 
                                                     <div>
 
-                                                        <?php
-                                                        $servername = "localhost";
-                                                        $username = "root";
-                                                        $password = "";
-                                                        $dbname = "moodle";
-
-                                                        //Create connection
-                                                        $conn = mysqli_connect($servername, $username, $password, $dbname);
-                                                        //check connection
-                                                        if (!$conn) {
-
-                                                            die("Connection failed:" . mysqli_connect_error());
-                                                        }
-                                                        $sql = "SELECT course_id,course_name FROM module";
-                                                        $result = mysqli_query($conn, $sql);
-
-                                                        if (mysqli_num_rows($result) > 0) {
-
-                                                            echo '<table class="table table-srtiped"><tr><th>ID</th><th>Course</th><th> </th></tr>'; //table h
-
-
-                                                            while ($row = mysqli_fetch_assoc($result)) {
-                                                                echo '<tr><td>' . $row["course_id"] . '</td><td>' . $row["course_name"];
-                                                                echo( '</td><td><a href="students.php?id=' . "'" . $row["course_id"] . "'" . '">Enroll</a></td></tr>' );
-                                                            }
-
-                                                            //echo ($_SESSION["user"]);
-                                                        } else {
-                                                            echo "0 results";
-                                                        }
-                                                        mysqli_close($conn);
-                                                        ?>
-                                                        <div>
-                                                        <?php
-                                                        $enrolled = $_GET['id'];
-                                                        if ($enrolled != NULL) {
-                                                            //echo $enrolled;
-
-                                                            $conn = new mysqli('localhost', 'root', '', 'moodle');
-                                                            if ($conn->connect_error) {
-                                                                die("Connection failed: " . $conn->connect_error);
-                                                            }
-                                                            $sql = "INSERT INTO enrollment (course_id,student_id)VALUES (" . $enrolled . ",'" . $_SESSION["user"] . "')";
-
-                                                            if ($conn->query($sql) === TRUE) {
-                                                                //  echo "Enrollent successfull";
-                                                            } else {
-                                                                echo "Error: " . $sql . "<br>" . $conn->error;
-                                                            }
-
-                                                            $conn->close();
-                                                        }
-                                                        ?>
+                                                     
                                                         </div>
                                                     </div>
 
